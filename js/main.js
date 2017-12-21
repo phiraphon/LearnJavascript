bb11ec0507414dc9bee2854dc4245016808cafca
phiraphon <t.phiraphon@gmail.com>
2017-12-21 09:41:52 +0700

    create condition array object

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
-var price_shirt = "true";
-var price_swimsuit = "5";
-console.log(5&1);
+
+function firstFunction( wid, hei){
+    var width = wid;
+    var height = hei;
+    var area = width * height;
+    return area;
+}
+var test = {firstname:"Phiraphon",
+             lastname:"Tunjaya", 
+             age:40};
+var grade;
+var point = 85;
+
+// if(point >= 80){
+//     grade = "A";
+// } else if(){
+//     grade = "B+"
+// } else{ 
+//     grade = "F";
+// }
+
+switch(point){
+    case 80:
+        grade = "A";
+        break;
+    case 75:
+        grade = "B+";
+        break;
+}
+console.log(grade);
 
 
+// console.log(firstFunction);
 
 // console.log(check);
 
